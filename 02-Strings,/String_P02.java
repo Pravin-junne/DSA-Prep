55fb4c6d0d9354fe1c1708deb386cbe8370bdf00
Pravin junne <128027999+Pravin-junne@users.noreply.github.com>
2026-06-10 22:45:38 +0530

    Check Palindrome String

---
+++ 02-Strings,/String_P02.java
@@ -0,0 +1,32 @@
+//Check Palindrome String
+
+public class String_P02 {
+
+    static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        
+        String str = "madam";
+
+        if (isPalindrome(str)) {
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not Palindrome");
+        }
+    }    
+}
